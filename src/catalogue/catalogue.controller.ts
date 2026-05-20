8e77a91c7c0400e333e8683e91193322a8e77641
fabierv <fabie.rav@gmail.com>
2026-05-20 18:37:27 +0300

    ajout page catalogue

--- src/catalogue/catalogue.controller.ts
+++ src/catalogue/catalogue.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
-import { CatalogueService } from './catalogue.service';
+import {
+  Controller,
+  Get,
+  Param,
+  Res,
+  NotFoundException,
+  ParseIntPipe,
+} from '@nestjs/common';
 
-@Controller('catalogues')
+import { PrismaService } from 'src/user/prisma.service';
+import * as path from 'path';
+import { Response } from 'express';
+
+@Controller('catalogue')
 export class CatalogueController {
-  constructor(private readonly catalogueService: CatalogueService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get('public')
-  getPublicCatalogues() {
-    return this.catalogueService.getCataloguesByCategories();
+  @Get('download/:id')
+  async downloadCatalogue(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: any,
+  ) {
+    console.log('-------------------' + id);
+    const catalogue = await this.prisma.catalogue.findUnique({
+      where: {
+        id_catalogue: id,
+      },
+    });
+
+    if (!catalogue || !catalogue.nom_lien_pdf) {
+      throw new NotFoundException('PDF introuvable');
+    }
+    console.log('process.cwd():', process.cwd());
+    console.log('DB path:', catalogue.nom_lien_pdf);
+
+    const filePath = path.join(
+      process.cwd(),
+      'uploads/catalogues/' + catalogue.nom_lien_pdf,
+    );
+
+    return res.download(filePath);
   }
 }
